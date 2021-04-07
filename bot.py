6c816aac6dc3a10e9e20efc843be4c22e2f6a826
Alan Thomas Shaji <50659499+shaji-stark@users.noreply.github.com>
2021-04-07 22:59:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import pandas as pd
 from datetime import datetime
 
 def sign_in(meetingid, pswd):
-    subprocess.call(r'C:\\Users\capta\AppData\Roaming\Zoom\bin\Zoom.exe')
+    subprocess.call(r'C:\\Users\capta\AppData\Roaming\Zoom\bin\Zoom.exe') #edit this to your respective paths to the zoom app
 
     time.sleep(5)
     
